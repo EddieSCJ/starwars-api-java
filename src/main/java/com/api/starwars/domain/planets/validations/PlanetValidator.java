afa28467615002e85703fa60bf72fe2ba261475f
Edcleidson Junior <eddieprofessionalmail@gmail.com>
2022-02-22 05:47:42 -0300

    feature: creating field validations

---
+++ src/main/java/com/api/starwars/domain/planets/validations/PlanetValidator.java
@@ -0,0 +1,7 @@
+package com.api.starwars.domain.planets.validations;
+
+import com.api.starwars.commons.validations.AbstractValidator;
+
+public class PlanetValidator extends AbstractValidator {
+}
+
