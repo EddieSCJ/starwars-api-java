97948b3172ae7b1782ebd8588c9b50506567af43
Edcleidson Junior <eddieprofessionalmail@gmail.com>
2022-03-01 10:27:29 -0300

    refactor: adjusting equals and hashcode

--- src/main/java/com/api/starwars/domain/planets/model/domain/Planet.java
+++ src/main/java/com/api/starwars/domain/planets/model/domain/Planet.java
@@ -1,3 +1,6 @@
 package com.api.starwars.domain.planets.model.domain;
 
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = false)
 public record Planet(String id, String name, String[] climate, String[] terrain, Integer movieAppearances, Long cacheInDays) {}
