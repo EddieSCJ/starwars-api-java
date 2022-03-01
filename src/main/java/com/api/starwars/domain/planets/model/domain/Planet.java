6c97d741e17090d483ed270e4f3983e36edb553b
Edcleidson Junior <eddieprofessionalmail@gmail.com>
2022-03-01 10:30:49 -0300

    refactor: adjusting equals and hashcode

--- src/main/java/com/api/starwars/domain/planets/model/domain/Planet.java
+++ src/main/java/com/api/starwars/domain/planets/model/domain/Planet.java
@@ -1,6 +1,23 @@
 package com.api.starwars.domain.planets.model.domain;
 
-import lombok.EqualsAndHashCode;
+import java.util.Arrays;
+import java.util.Objects;
 
-@EqualsAndHashCode(callSuper = false)
-public record Planet(String id, String name, String[] climate, String[] terrain, Integer movieAppearances, Long cacheInDays) {}
+public record Planet(String id, String name, String[] climate, String[] terrain, Integer movieAppearances, Long cacheInDays) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Planet planet = (Planet) o;
+        return Objects.equals(id, planet.id) && Objects.equals(name, planet.name) && Arrays.equals(climate, planet.climate) && Arrays.equals(terrain, planet.terrain) && Objects.equals(movieAppearances, planet.movieAppearances) && Objects.equals(cacheInDays, planet.cacheInDays);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, name, movieAppearances, cacheInDays);
+        result = 31 * result + Arrays.hashCode(climate);
+        result = 31 * result + Arrays.hashCode(terrain);
+        return result;
+    }
+}
