98f5985b5fa5d49137561c6d843abeb70a9e7a64
Edcleidson Junior <eddieprofessionalmail@gmail.com>
2022-03-01 02:11:59 -0300

    refactor: adjusting test layers and descriptions

--- src/test/java/com/api/starwars/domain/planets/repositories/PlanetRepositoryTest.java
+++ src/test/java/com/api/starwars/domain/planets/repositories/PlanetRepositoryTest.java
@@ -5,6 +5,8 @@ import com.api.starwars.domain.planets.model.domain.Planet;
 import com.api.starwars.domain.planets.model.mongo.MongoPlanet;
 import com.mongodb.client.result.DeleteResult;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -38,87 +40,119 @@ public class PlanetRepositoryTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    public void count() {
-        Criteria criteria = where(FIELD_ID).exists(true);
+    @Nested
+    @DisplayName("Contar documentos.")
+    class Count {
+        @Test
+        @DisplayName("Deve retornar a quantidade de documentos na colecao")
+        public void count() {
+            Criteria criteria = where(FIELD_ID).exists(true);
 
-        planetRepository.count();
-        verify(mongoTemplate, times(1)).count(eq(query(criteria)), eq(MongoPlanet.class));
-    }
-
-    @Test
-    public void find_by_id_successful() {
-        Criteria criteria = where(FIELD_ID).is(FAKE_ID);
-        when(mongoTemplate.findOne(eq(query(criteria)), eq(MongoPlanet.class))).thenReturn(Domain.getRandomMongoPlanet());
-
-        planetRepository.findById(FAKE_ID);
-        verify(mongoTemplate, times(1)).findOne(eq(query(criteria)), eq(MongoPlanet.class));
-    }
-
-    @Test
-    public void find_by_id_fail_when_not_found() {
-        Criteria criteria = where(FIELD_ID).is(FAKE_ID);
-        when(mongoTemplate.findOne(query(criteria), eq(any()))).thenReturn(Optional.empty());
-
-        assertThrows(HttpNotFoundException.class, () -> planetRepository.findById(FAKE_ID));
-        verify(mongoTemplate, times(1)).findOne(eq(query(criteria)), eq(MongoPlanet.class));
-
-    }
-
-    @Test
-    public void find_by_name_successful() {
-        final String FAKE_NAME = "fake_name";
-        Criteria criteria = getFindByNameCriteria(FAKE_NAME);
-
-        planetRepository.findByName(FAKE_NAME);
-        verify(mongoTemplate, times(1)).findOne(eq(query(criteria)), eq(MongoPlanet.class));
-    }
-
-    @Test
-    public void find_by_name_fail_when_not_found() {
-        final String FAKE_NAME = "fake_name";
-        Criteria criteria = getFindByNameCriteria(FAKE_NAME);
-
-        when(mongoTemplate.findOne(query(criteria), eq(any()))).thenReturn(Optional.empty());
-
-        planetRepository.findByName(FAKE_NAME);
-        verify(mongoTemplate, times(1)).findOne(eq(query(criteria)), eq(MongoPlanet.class));
-    }
-
-    @Test
-    public void save_successful() {
-        LocalDateTime dateTime = LocalDateTime.now();
-
-        try (MockedStatic<LocalDateTime> mock = Mockito.mockStatic(LocalDateTime.class)) {
-            mock.when(LocalDateTime::now).thenReturn(dateTime);
-
-            Planet planet = Domain.getRandomPlanet();
-            planetRepository.save(planet);
-
-            verify(mongoTemplate, times(1)).save(eq(MongoPlanet.fromDomain(planet)));
+            planetRepository.count();
+            verify(mongoTemplate, times(1)).count(eq(query(criteria)), eq(MongoPlanet.class));
         }
 
     }
 
-    @Test
-    public void delete_by_id_successful() {
-        Criteria criteria = where(FIELD_ID).is(FAKE_ID);
+    @Nested
+    @DisplayName("Buscar por Id.")
+    class FindById {
+        @Test
+        @DisplayName("Deve retornar um Optional nao vazio de MongoPlanet da database.")
+        public void successful() {
+            Criteria criteria = where(FIELD_ID).is(FAKE_ID);
+            when(mongoTemplate.findOne(eq(query(criteria)), eq(MongoPlanet.class))).thenReturn(Domain.getRandomMongoPlanet());
 
-        when(mongoTemplate.remove(criteria)).thenReturn(DeleteResult.acknowledged(1L));
+            planetRepository.findById(FAKE_ID);
+            verify(mongoTemplate, times(1)).findOne(eq(query(criteria)), eq(MongoPlanet.class));
+        }
 
-        planetRepository.deleteById(FAKE_ID);
-        verify(mongoTemplate, times(1)).remove(criteria);
+        @Test
+        @DisplayName("Deve estourar 404 quando nao encontrar nenhum planeta com o id passado na database.")
+        public void when_not_found() {
+            Criteria criteria = where(FIELD_ID).is(FAKE_ID);
+            when(mongoTemplate.findOne(query(criteria), eq(any()))).thenReturn(Optional.empty());
+
+            assertThrows(HttpNotFoundException.class, () -> planetRepository.findById(FAKE_ID));
+            verify(mongoTemplate, times(1)).findOne(eq(query(criteria)), eq(MongoPlanet.class));
+
+        }
+
     }
 
-    @Test()
-    public void delete_by_id_fail_when_not_found() {
-        Criteria criteria = where(FIELD_ID).is(FAKE_ID);
-        when(mongoTemplate.remove(criteria)).thenReturn(DeleteResult.acknowledged(0L));
+    @Nested
+    @DisplayName("Buscar por nome.")
+    class FindByName {
+        @Test
+        @DisplayName("Deve retornar um Optional nao vazio")
+        public void successful() {
+            final String FAKE_NAME = "fake_name";
+            Criteria criteria = getFindByNameCriteria(FAKE_NAME);
+            when(mongoTemplate.findOne(query(criteria), MongoPlanet.class)).thenReturn(Domain.getRandomMongoPlanet());
 
-        assertThrows(HttpNotFoundException.class, () -> planetRepository.deleteById(FAKE_ID));
+            planetRepository.findByName(FAKE_NAME);
+            verify(mongoTemplate, times(1)).findOne(eq(query(criteria)), eq(MongoPlanet.class));
+        }
+
+        @Test
+        @DisplayName("Deve estourar 404 quando nao encontrar nenhum planeta na database com o nome passado.")
+        public void fail_when_not_found() {
+            final String FAKE_NAME = "fake_name";
+            Criteria criteria = getFindByNameCriteria(FAKE_NAME);
+
+            when(mongoTemplate.findOne(query(criteria), eq(any()))).thenReturn(Optional.empty());
+
+            assertThrows(HttpNotFoundException.class, () -> planetRepository.findByName(FAKE_NAME));
+            verify(mongoTemplate, times(1)).findOne(eq(query(criteria)), eq(MongoPlanet.class));
+        }
+
     }
 
-    public Criteria getFindByNameCriteria(String FAKE_NAME) {
+    @Nested
+    @DisplayName("Salvar.")
+    class Save {
+        @Test
+        @DisplayName("Deve salvar com sucesso um MongoPlanet")
+        public void successful() {
+            LocalDateTime dateTime = LocalDateTime.now();
+
+            try (MockedStatic<LocalDateTime> mock = Mockito.mockStatic(LocalDateTime.class)) {
+                mock.when(LocalDateTime::now).thenReturn(dateTime);
+
+                Planet planet = Domain.getRandomPlanet();
+                planetRepository.save(planet);
+
+                verify(mongoTemplate, times(1)).save(eq(MongoPlanet.fromDomain(planet)));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("Deletar por Id.")
+    class DeleteById {
+
+        @Test
+        @DisplayName("Deve buscar por id, encontrar e deletar com sucesso o MongoPlanet.")
+        public void successful() {
+            Criteria criteria = where(FIELD_ID).is(FAKE_ID);
+
+            when(mongoTemplate.remove(criteria)).thenReturn(DeleteResult.acknowledged(1L));
+
+            planetRepository.deleteById(FAKE_ID);
+            verify(mongoTemplate, times(1)).remove(criteria);
+        }
+
+        @Test
+        @DisplayName("Deve estourar um 404 ao tentar deletar pois nao existe documento com o id passado.")
+        public void fail_when_not_found() {
+            Criteria criteria = where(FIELD_ID).is(FAKE_ID);
+            when(mongoTemplate.remove(criteria)).thenReturn(DeleteResult.acknowledged(0L));
+
+            assertThrows(HttpNotFoundException.class, () -> planetRepository.deleteById(FAKE_ID));
+        }
+    }
+
+    private Criteria getFindByNameCriteria(String FAKE_NAME) {
         Criteria lowercase = where(FIELD_NAME).is(FAKE_NAME.toLowerCase());
         Criteria uppercase = where(FIELD_NAME).is(FAKE_NAME.toUpperCase());
         Criteria capitalized = where(FIELD_NAME).is(StringUtils.capitalize(FAKE_NAME));
