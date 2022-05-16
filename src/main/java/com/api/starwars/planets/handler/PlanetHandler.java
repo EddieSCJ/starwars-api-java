906beba45bbdaa676ab9b5d0e044fa938e446e9d
edcleidson.junior <edcleidson.junior@b2wdigital.com>
2022-05-16 05:07:43 -0300

    feature: enabling authorizations by user authorities

--- src/main/java/com/api/starwars/planets/handler/PlanetHandler.java
+++ src/main/java/com/api/starwars/planets/handler/PlanetHandler.java
@@ -15,8 +15,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.IOException;
 
@@ -27,7 +29,6 @@ import static com.api.starwars.planets.enums.OperationsEnum.*;
 @RequestMapping(value = Constants.PLANETS_ENDPOINT)
 @Tag(name = "Planets")
 public class PlanetHandler {
-
     private final IPlanetService planetService;
 
     @Autowired
@@ -36,15 +37,17 @@ public class PlanetHandler {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('PLANET_LIST')")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PageResponse.class)))
     public ResponseEntity<PageResponse<PlanetJson>> getAll(
             @RequestParam(name = "page", defaultValue = "0") Integer page,
             @RequestParam(name = "order", defaultValue = "name") String order,
             @RequestParam(name = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(name = "size", defaultValue = "15") Integer size
+            @RequestParam(name = "size", defaultValue = "15") Integer size,
+            HttpServletRequest request
     ) {
         log.info("Iniciando busca por todos os planetas. page: {}, order: {}, direction: {}, size: {}.", page, order, direction, size);
-        LoggerUtils.setOperationInfoIntoMDC(GET_PLANETS_PAGE);
+        LoggerUtils.setOperationInfoIntoMDC(GET_PLANETS_PAGE, request);
 
         Page<Planet> planets = planetService.findAll(page, order, direction, size);
         Page<PlanetJson> pageResponse = planets.map(PlanetJson::fromDomain);
@@ -54,13 +57,15 @@ public class PlanetHandler {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('PLANET_LIST')")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PlanetJson.class)))
     public ResponseEntity<PlanetJson> getByID(
             @PathVariable String id,
-            @RequestParam(name = "cacheInDays", defaultValue = "0") Long cacheInDays
+            @RequestParam(name = "cacheInDays", defaultValue = "0") Long cacheInDays,
+            HttpServletRequest request
     ) throws IOException, InterruptedException {
         log.info("Iniciando busca de planeta por id. id: {}. cacheInDays: {}.", id, cacheInDays);
-        LoggerUtils.setOperationInfoIntoMDC(id, GET_PLANET_BY_ID);
+        LoggerUtils.setOperationInfoIntoMDC(id, GET_PLANET_BY_ID, request);
 
         Planet planet = planetService.findById(id, cacheInDays);
         PlanetJson planetJson = PlanetJson.fromDomain(planet);
@@ -70,13 +75,15 @@ public class PlanetHandler {
     }
 
     @GetMapping("/search")
+    @PreAuthorize("hasAuthority('PLANET_LIST')")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PlanetJson.class)))
     public ResponseEntity<PlanetJson> getByName(
             @RequestParam String name,
-            @RequestParam(defaultValue = "0") Long cacheInDays
+            @RequestParam(defaultValue = "0") Long cacheInDays,
+            HttpServletRequest request
     ) throws IOException, InterruptedException {
         log.info("Iniciando busca de planeta pelo nome. name: {}. cacheInDays: {}.", name, cacheInDays);
-        LoggerUtils.setOperationInfoIntoMDC(name, GET_PLANET_BY_NAME);
+        LoggerUtils.setOperationInfoIntoMDC(name, GET_PLANET_BY_NAME, request);
 
         Planet planet = planetService.findByName(name, cacheInDays);
         PlanetJson planetJson = PlanetJson.fromDomain(planet);
@@ -86,10 +93,11 @@ public class PlanetHandler {
     }
 
     @PutMapping
+    @PreAuthorize("hasAuthority('PLANET_UPDATE')")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PlanetJson.class)))
-    public ResponseEntity<PlanetJson> updateById(@RequestBody PlanetJson planetJson) {
+    public ResponseEntity<PlanetJson> updateById(@RequestBody PlanetJson planetJson, HttpServletRequest request) {
         log.info("Iniciando atualizacao de planeta pelo id. id: {}", planetJson.getId());
-        LoggerUtils.setOperationInfoIntoMDC(planetJson.getId(), UPDATE_PLANET);
+        LoggerUtils.setOperationInfoIntoMDC(planetJson.getId(), UPDATE_PLANET, request);
 
         Planet planet = planetService.updateById(planetJson.getId(), planetJson.toDomain());
 
@@ -98,10 +106,11 @@ public class PlanetHandler {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('PLANET_CREATE')")
     @ApiResponse(responseCode = "201", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PlanetJson.class)))
-    public ResponseEntity<PlanetJson> post(@Valid @RequestBody PlanetJson planet) {
+    public ResponseEntity<PlanetJson> post(@Valid @RequestBody PlanetJson planet, HttpServletRequest request) {
         log.info("Iniciando cadastro de planeta por nome. name: {}.", planet.getName());
-        LoggerUtils.setOperationInfoIntoMDC(planet.getId(), DELETE_PLANET);
+        LoggerUtils.setOperationInfoIntoMDC(planet.getId(), DELETE_PLANET, request);
 
         Planet domainPlanet = planetService.save(planet.toDomain());
         PlanetJson planetJson = PlanetJson.fromDomain(domainPlanet);
@@ -111,10 +120,11 @@ public class PlanetHandler {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('PLANET_DELETE')")
     @ApiResponse(responseCode = "204", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
-    public ResponseEntity<?> delete(@PathVariable String id) {
+    public ResponseEntity<?> delete(@PathVariable String id, HttpServletRequest request) {
         log.info("Iniciando exclusao de planeta pelo id: {}.", id);
-        LoggerUtils.setOperationInfoIntoMDC(id, DELETE_PLANET);
+        LoggerUtils.setOperationInfoIntoMDC(id, DELETE_PLANET, request);
 
         planetService.deleteById(id);
 
