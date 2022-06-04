17bd23d78d71ea3fdf50a6fcdff6345dd54a2147
Edcleidson Junior <eddieprofessionalmail@gmail.com>
2022-06-03 21:06:51 -0300

    refactor: adjusting bash to create queue with deduplication enabled

--- localstack/create-queue.sh
+++ localstack/create-queue.sh
@@ -7,7 +7,7 @@ export SQS_REGION=us-east-1
 # AWS_PROFILE needs to be set, but dev isn't actually used
 
 # Create the queue
-aws --endpoint-url $SQS_ENDPOINT sqs create-queue --queue-name planet-delete.fifo --region us-east-1 --attributes FifoQueue=true
+aws --endpoint-url $SQS_ENDPOINT sqs create-queue --queue-name planet-delete.fifo --region us-east-1 --attributes '{"FifoQueue":"true", "ContentBasedDeduplication":"true"}' --output table | cat
 
 # Clear any existing messages from the queue
 aws --endpoint-url $SQS_ENDPOINT sqs purge-queue --region us-east-1 --queue-url $SQS_QUEUE_URL
