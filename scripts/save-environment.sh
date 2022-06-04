7147fabf5322dd8c9e1caadfdb72473e05ad98e7
Lucas Rego <lucas.raziel@gmail.com>
2022-06-04 15:07:24 -0300

    fix docker save

--- scripts/save-environment.sh
+++ scripts/save-environment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Run the app using local stack SQS and S3
 
-docker save -o /tmp/cache-docker/localstack.tar localstack/localstack
+docker save -o /tmp/cache-docker/localstack.tar docker.io/library/localstack/localstack:latest
 
-docker save -o  /tmp/cache-docker/mongo.tar mongo
+docker save -o  /tmp/cache-docker/mongo.tar docker.io/library/mongo:latest
